36976d70e3f21bcc5779bdc9ea9d9103b584c4a5
Alvinistan <alvinistan@students.nsbm.ac.lk>
2025-10-20 00:55:49 +0530

    Add Course price Section dashbd

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -32,19 +32,27 @@ const AddCourse = () => {
 
   return (
     <div className="h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pb-0">
-      <form>
-        <div>
+      <form className="flex flex-col gap-4 max-w-md w-full text-gray-500 ">
+        <div className="flex flex-col gap-1">
           <p>Course Title</p>
           <input className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500" required onChange={e => setCourseTitle(e.target.value)} type="text" placeholder="Type here" value={courseTitle}/>
         </div>
 
         {/* Quill Sectors and import Snow.css in app.js */}
-        <div>
+        <div className="flex flex-col gap-1">
           <p>Course Description</p>
           <div ref={editorRef}>
 
           </div>
         </div>
+        {/* Course Price */}
+        <div>
+          <div className="flex flex-col gap-1">
+            <p>Course Price</p>
+            <input className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500 gap-1" required type="text" name="" id=""/>
+          </div>
+        </div>
+
       </form>
     </div>
   );
