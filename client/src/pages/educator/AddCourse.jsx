3277ed8ef1e14620734f1557c227e9f2af3b868b
Alvinistan <alvinistan@students.nsbm.ac.lk>
2025-10-19 22:43:02 +0530

    initiate Quill only once

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -23,6 +23,16 @@ const AddCourse = () => {
     isPreviewFree: ''
   })
 
+  useEffect(()=> {
+    //initiate Quill only once
+    if(!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: 'snow'
+      })
+    }
+
+  },[])
+
 
 
 
