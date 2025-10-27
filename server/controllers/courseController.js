b7d13f609e948fd85e367e19203f8020ca19de52
Alvinistan <alvinistan@students.nsbm.ac.lk>
2025-10-27 14:48:02 +0530

    Get Course by Id

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -9,3 +9,26 @@ export const getAllCourses = async (req,res)=> {
         res.json({success: false, message: error.message})
     }
 }
+
+// Get Course by Id
+export const getCourseId = async (req,res)=>{
+    const {id} = req.params
+
+    try {
+        const courseData = await Course.findById(id).populate({path: 'educator'})
+
+        // remove lectureUrl if isPreviewFree is false
+        courseData.courseContent.forEach(chapter =>{
+            chapter.chapterContent.forEach(lecture => {
+                if(!lecture.isPreviewFree){
+                    lecture.lectureUrl = "";
+                }
+            }) 
+        })
+
+        res.json({ success: true, courseData })
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+
+}
